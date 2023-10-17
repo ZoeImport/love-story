881acecaebbfc9f2a4416353adaef86bd537bf2e
ZoeImport <nokeruila@gmail.com>
2023-10-17 12:01:06 +0000

    done for void pointer reference and begin with object before friend

---
+++ C++_NewBegin/object/build/CMakeFiles/object.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zoe/Learning/Notes/C++_NewBegin/object/Student.cpp" "CMakeFiles/object.dir/Student.cpp.o" "gcc" "CMakeFiles/object.dir/Student.cpp.o.d"
+  "/home/zoe/Learning/Notes/C++_NewBegin/object/Teacher.cpp" "CMakeFiles/object.dir/Teacher.cpp.o" "gcc" "CMakeFiles/object.dir/Teacher.cpp.o.d"
+  "/home/zoe/Learning/Notes/C++_NewBegin/object/main.cpp" "CMakeFiles/object.dir/main.cpp.o" "gcc" "CMakeFiles/object.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++_NewBegin/object/build/CMakeFiles/object.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/object.dir/Student.cpp.o"
+  "CMakeFiles/object.dir/Student.cpp.o.d"
+  "CMakeFiles/object.dir/Teacher.cpp.o"
+  "CMakeFiles/object.dir/Teacher.cpp.o.d"
+  "CMakeFiles/object.dir/main.cpp.o"
+  "CMakeFiles/object.dir/main.cpp.o.d"
+  "object"
+  "object.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/object.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
