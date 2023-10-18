8ff6e595cb544de742b9be55fcb9adc5276779d4
ZoeImport <nokeruila@gmail.com>
2023-10-18 09:44:51 +0000

    deelte

--- C++_NewBegin/object/Student.h
+++ C++_NewBegin/object/Student.h
@@ -1,21 +1,21 @@
 #ifndef _STU_CLASS_
 #define _STU_CLASS_
 #include<string>
-using namespace std;
+// using namespace std;
 
 class Student{
     private:
-        string name;
+        std:: string name;
         mutable int age;
         char gender;
     public:
         Student();//空参构造
-        Student(string name,int age,char gender);
+        Student(std::string name,int age,char gender);
         ~Student();
         void setAge(int age);
         int getAge()const;
-        void setName(string name);
-        string getName();
+        void setName(std::string name);
+        std::string getName();
         void showAge() const;
 };
 
