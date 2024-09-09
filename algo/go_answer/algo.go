1e573b5b2ab59e3e8227a2f45e9ecbb73ddc09d2
zoeimport <nokeruila@gmail.com>
2024-09-09 20:55:22 +0800

    更新 algo.go

--- algo/go_answer/algo.go
+++ algo/go_answer/algo.go
@@ -345,3 +345,18 @@ func maxScore2(str string) int {
 	}
 	return result
 }
+
+func search(nums []int, target int) int {
+    l,r:=0,len(nums)-1
+    for l<=r {
+        mid:=l+(r-l)>>1
+        if target>nums[mid] {
+            l=mid+1
+        }else if target<nums[mid]{
+            r=mid-1
+        }else {
+            return mid
+        }
+    }
+    return -1
+}
